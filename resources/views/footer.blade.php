2cd814550106b0bf64c793daa92eb9223f552c69
jamalhamliri <hamliri2014@gmail.com>
2022-08-12 02:56:22 +0100

    First Commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="text-center text-white m-3" style="background-color: #f1f1f1;">
+ 
+  <!-- Copyright -->
+  <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2022 Copyright:
+    <a class="text-dark" href="https://app.test/">E-commerce</a>
+  </div>
+  <!-- Copyright -->
+</footer>
